bc946c7e4e488c859b46036c68d8302ae5a3b2ef
mariodmpereira <mariodmpereira@gmail.com>
2021-12-08 20:57:28 +0000

    Create User model

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
