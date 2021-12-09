55d8d28b000eddddc1a1f440f073219ae37db4ca
mariodmpereira <mariodmpereira@gmail.com>
2021-12-09 21:02:37 +0000

    Configure Passport

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-const moongoose = require('mongoose');
-const Schema = moongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
