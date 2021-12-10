8864344c1ef4fa40530c8456d9b6bfaa699828e1
mariodmpereira <mariodmpereira@gmail.com>
2021-12-10 19:13:14 +0000

    Register Form

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
 
 mongoose.connect(envs.MONGODB_URL, {
     useNewUrlParser: true,
@@ -69,8 +70,9 @@ app.get('/fakeUser', async (req, res) => {
     res.send(newUser)
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes);
+app.use('/campgroundRoutes', campgroundRoutes)
+app.use('/campgroundRoutes/:id/reviewRoutes', reviewRoutes)
 
 app.get('/', (req, res) => {
     res.render('home')
