fdf9a209f0f677349862d3e080250b64d52cfe24
mariodmpereira <mariodmpereira@gmail.com>
2021-12-17 23:20:22 +0000

    Finish 'Section 51: YelpCamp: Adding in Authentication'

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
-const { campgroundSchema } = require("../schemas.js");
-const { isLoggedIn } = require("../middleware");
+const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
-const ExpressError = require('../utils/ExpressError');
-const Campgrounds = require("../models/campground");
-
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    next();
-}
+const Campground = require("../models/campground");
 
 router.get('/', catchAsync(async (req, res) => {
-    const campgrounds = await Campgrounds.find({});
+    const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 }))
 
@@ -26,14 +15,21 @@ router.get('/new', isLoggedIn, (req, res) => {
 })
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    const campground = new Campgrounds(req.body.campground);
+    const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save()
     req.flash('success', 'Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.get('/:id', isLoggedIn, catchAsync(async (req, res) => {
-    const campground = await Campgrounds.findById(req.params.id).populate('reviews');
+router.get('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds')
@@ -41,8 +37,9 @@ router.get('/:id', isLoggedIn, catchAsync(async (req, res) => {
     res.render('campgrounds/show', { campground })
 }))
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
-    const campground = await Campgrounds.findById(req.params.id);
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    let { id } = req.params;
+    const campground = await Campground.findById(id);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds')
@@ -50,16 +47,16 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-router.put('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campgrounds.findByIdAndUpdate(id, { ...req.body.campground })
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
-    await Campgrounds.findByIdAndDelete(id);
+    await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground!')
     res.redirect('/campgrounds');
 }))
--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
         const randomCity = sample(cities);
         const randomPrice = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: envs.USER_ID,
             location: `${randomCity.city}, ${randomCity.state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/190727/',
